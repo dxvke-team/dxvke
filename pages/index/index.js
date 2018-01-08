81d17c0b11fbeb68c080d4b61030d498608ad717
jingloveing <1172881783@qq.com>
2018-01-08 14:12:56 +0800

    第一次提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -85,6 +85,10 @@ Page({
     wx.navigateTo({
       url: "../fanswefare/fanswefare"
     })
+  },
+  toGoodsDetail:function(e){
+    wx.navigateTo({
+      url: "../goodsDetail/goodsDetail"
+    })
   }
-
 })
